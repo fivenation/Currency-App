72f89c176b43636a909631b669b3690eee09a366
Andrey Pyatnitsin <86624715+fivenation@users.noreply.github.com>
2023-08-18 21:11:14 +0300

    fix theme manager

--- lib/domain/dependencies/service_locator.config.dart
+++ lib/domain/dependencies/service_locator.config.dart
@@ -12,6 +12,7 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
+import '../../presentation/theme/theme_manager.dart' as _i4;
 import '../../utils/scaffold_messenger.dart' as _i3;
 
 extension GetItInjectableX on _i1.GetIt {
@@ -26,6 +27,7 @@ extension GetItInjectableX on _i1.GetIt {
       environmentFilter,
     );
     gh.lazySingleton<_i3.Messenger>(() => _i3.Messenger());
+    gh.lazySingleton<_i4.ThemeManager>(() => _i4.ThemeManager());
     return this;
   }
 }
--- lib/presentation/application.dart
+++ lib/presentation/application.dart
@@ -1,5 +1,7 @@
 import 'package:currency_app/domain/dependencies/service_locator.dart';
 import 'package:currency_app/presentation/common/loading_page.dart';
+import 'package:currency_app/presentation/theme/theme_data.dart';
+import 'package:currency_app/presentation/theme/theme_manager.dart';
 import 'package:currency_app/utils/l10n/S.dart';
 import 'package:currency_app/utils/l10n/app_localizations/app_localizations.dart';
 import 'package:currency_app/utils/logger.dart';
@@ -7,6 +9,7 @@ import 'package:currency_app/utils/scaffold_messenger.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 
 class RunApplication extends StatefulWidget {
   const RunApplication({Key? key}) : super(key: key);
@@ -25,25 +28,35 @@ class _RunApplicationState extends State<RunApplication> {
 
     /// TODO --> Change, when add DI
     final messenger = getIt<Messenger>();
-    return MaterialApp(
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: getIt<ThemeManager>()),
+      ],
+      child: MaterialApp(
 
-      // Scaffold Messenger
-      scaffoldMessengerKey: messenger.key,
+        // Theme
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        themeMode: context.read<ThemeManager>().themeMode,
 
-      // Localization
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
+        // Scaffold Messenger
+        scaffoldMessengerKey: messenger.key,
 
-      home: ScreenUtilInit(
-        splitScreenMode: true,
-        designSize: const Size(390 , 844),
-        builder: (_,__) =>  FutureBuilder(
-          future: getIt.allReady(),
-          builder: (context, snapshot) {
-            logger.d(S.of(context).appTitle);
-            return !snapshot.hasData ? const LoadingPage()
-                : Text(S.of(context).appTitle);
-          }
+        // Localization
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+
+        home: ScreenUtilInit(
+          splitScreenMode: true,
+          designSize: const Size(390 , 844),
+          builder: (_,__) =>  FutureBuilder(
+            future: getIt.allReady(),
+            builder: (context, snapshot) {
+              logger.d(S.of(context).appTitle);
+              return !snapshot.hasData ? const LoadingPage()
+                  : Text(S.of(context).appTitle);
+            }
+          ),
         ),
       ),
     );
