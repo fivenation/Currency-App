32b84d2892b7398d6d105c2756607079f52c5b9a
Andrey Pyatnitsin <86624715+fivenation@users.noreply.github.com>
2023-08-22 18:16:53 +0300

    fix summary_hive

--- lib/data/sources/summary/local/summary_local.dart
+++ lib/data/sources/summary/local/summary_local.dart
@@ -2,6 +2,6 @@ import 'package:currency_app/domain/models/summary/summary_data.dart';
 
 abstract class SummaryLocal {
   Future<List<SummaryData>> getAll();
-  Future<List<SummaryData>> saveAll();
-  Future<SummaryData> changeFavorite(String name);
+  Future<List<SummaryData>> saveAll(List<SummaryData> list);
+  Future<SummaryData> update(SummaryData data);
 }
