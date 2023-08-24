5da938a38525c2777ca00d5d878601d099a403ce
Andrey Pyatnitsin <86624715+fivenation@users.noreply.github.com>
2023-08-24 17:21:53 +0300

    feature firebase auth

---
+++ lib/domain/models/user/user_data.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_data.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_UserData _$$_UserDataFromJson(Map<String, dynamic> json) => _$_UserData(
+      id: json['id'] as String?,
+      email: json['email'] as String?,
+      username: json['username'] as String?,
+    );
+
+Map<String, dynamic> _$$_UserDataToJson(_$_UserData instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('id', instance.id);
+  writeNotNull('email', instance.email);
+  writeNotNull('username', instance.username);
+  return val;
+}
